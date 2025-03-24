b856641fce516063b24f76ce13eb4b6ea0e0629c
JSure-F <1207856991@qq.com>
2025-03-24 20:33:50 +0800

    rope shape change commit

--- RoPE1.py
+++ RoPE1.py
@@ -25,7 +25,7 @@ def apply_rotary_emb(xq, xk, freqs_cis):
     # xq_.shape = (batch, max_seq_len, num_heads, head_dim//2)
     xq_ = torch.view_as_complex(xq.reshape(*xq.shape[:-1], -1, 2))
     xk_ = torch.view_as_complex(xk.reshape(*xk.shape[:-1], -1, 2))
-    # （max_seq_len, head_dim -> (1, max_seq_len, 1, head_dim)
+    # （max_seq_len, head_dim//2) -> (1, max_seq_len, 1, head_dim//2)
     freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
     # 逐元素相乘的时候shape不变
     # view_as_real:（batch, max_seq_len, num_heads, head_dim//2） -> (batch, max_seq_len, num_heads, head_dim//2, 2)
@@ -52,3 +52,18 @@ xq_out, xk_out = apply_rotary_emb(xq, xk, freqs_cis)
 
 print("xq after rotary embedding:", xq_out.shape)
 print("xk after rotary embedding:", xk_out.shape)
+
+"""
+xq.shape = (batch, max_seq_len, num_heads, head_dim)
+after reshape (batch, max_seq_len, num_heads, head_dim // 2, 2)
+after view_as_complex (batch, max_seq_len, num_heads, head_dim//2) 抹掉最后一个2
+
+freq_cis.shape = （max_seq_len, head_dim//2)
+after reshape_for_broadcast shape= （1, max_seq_len, 1,  head_dim//2 )
+
+xq_ * freqs_cis：* 代表 per element 相乘，需要经过broadcast
+(batch, max_seq_len, num_heads, head_dim//2) * （1, max_seq_len, 1,  head_dim//2 ) -> (batch, max_seq_len, num_heads, head_dim//2) 
+
+after view as real (batch, max_seq_len, num_heads, head_dim//2, 2) 添加上最后一个2
+after flatten (batch, max_seq_len, num_heads, head_dim) 
+"""
